533f3ce1818f4621f9b2a7f99a43f4542c993d22
sandeep-testsigma <sandeepkumar.yadav@testsigma.com>
2025-06-27 14:03:01 +0530

    error

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -5,6 +6,7 @@ import "./utils/newrelic";
 
 function App() {
   const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -18,7 +20,7 @@ function App() {
       </div>
       <h1>React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCounts((count) => count + 1)}>
           count is {count}
         </button>
         <p>
